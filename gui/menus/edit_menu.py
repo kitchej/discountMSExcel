dfcc79d6c4530031978c8d9dd3cde38054f2bc1e
kitch <kitchej96@gmail.com>
2026-04-11 19:55:47 -0700

    Edit menu has finally been implemented

--- gui/menus/edit_menu.py
+++ gui/menus/edit_menu.py
@@ -1,19 +1,18 @@
-import tkinter.ttk as ttk
 import tkinter as tk
 
 class EditMenu(tk.Menu):
     def __init__(self, main_win):
         super().__init__(tearoff=0)
         self.main_win = main_win
-        self.add_command(label="Cut", accelerator="Ctrl+C", command=self.copy)
+        self.add_command(label="Copy", accelerator="Ctrl+C", command=self.copy)
         self.add_command(label="Cut", accelerator="Ctrl+X", command=self.cut)
-        self.add_command(label="Cut", accelerator="Ctrl+V", command=self.paste)
+        self.add_command(label="Paste", accelerator="Ctrl+V", command=self.paste)
 
     def copy(self):
-        pass
+        self.main_win.cell_area.multi_copy()
 
     def cut(self):
-        pass
+        self.main_win.cell_area.multi_cut()
 
     def paste(self):
-        pass
+        self.main_win.cell_area.multi_paste()
